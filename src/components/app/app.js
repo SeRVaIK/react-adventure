c64d6993972e94740ed4925434a8f383e02a16c3
sv.sinergium.ru <ovticalint@gmail.com>
2023-02-14 23:39:08 +0500

    init commit
    adding basic assembly

---
+++ src/components/app/app.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+import Header         from '../header';
+import RandomPlanet   from '../random-planet';
+import ItemList       from '../item-list';
+import PersonDetails  from '../person-details';
+
+import './app.css';
+import ErrorButton    from '../error-button';
+import ErrorIndicator from '../error-indicator';
+
+export default class App extends Component {
+
+  state = {
+    showRandomPlanet: true,
+    selectedPerson: null,
+  };
+
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };
+
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id,
+      hasError: false,
+    });
+  };
+  
+  componentDidCatch(error, errorInfo) {
+    console.log('componentDidCatch');
+    this.setState({hasError: true})
+  }
+  
+  render() {
+    
+    if (this.state.hasError) {
+      return <ErrorIndicator/>
+    }
+
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet/> :
+      null;
+
+    return (
+      <div className="stardb-app">
+        <Header />
+        { planet }
+
+        <div className='row mb2 button-row'>
+          <button
+              className="toggle-planet btn btn-warning btn-lg"
+              onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+          </button>
+          <ErrorButton/>
+        </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/planets-details/planets-details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './planets-details.css';
+
+const PlanetsDetails = () => {
+    return (
+        <div className='planets-details'>
+        </div>
+    );
+}
+
+export default PlanetsDetails;
